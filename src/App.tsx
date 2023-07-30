856dcf7fb7410c7bec19128e98f983c8a5e6bca3
StephanJ98 <diego.jeandon@gmail.com>
2023-07-30 13:14:26 +0200

    :tada: New Branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import Header from './components/Header/header'
-import DropDownBody from './components/DropDownBody'
-import styles from './App.module.css'
 
 function App() {
+
   return (
-    <div className={styles.App}>
-      <Header />
-      <DropDownBody />
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
-export default App;
+export default App
